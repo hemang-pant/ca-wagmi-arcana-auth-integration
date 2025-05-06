e7c8cd0720f18038c9e029562ab0461996580a5f
hemang <hemangpant2002@gmail.com>
2025-05-06 14:23:38 +0530

    updated @arcana/auth

--- example/src/utils/config.ts
+++ example/src/utils/config.ts
@@ -10,11 +10,11 @@ import {
 } from "wagmi/chains";
 import { injected } from "wagmi/connectors";
 import { ArcanaConnector } from "@arcana/auth-wagmi"
-import { AuthProvider } from "@arcana/auth"
+import { AuthProvider } from "@arcana/auth/esm";
 
 const auth = new AuthProvider(`xar_test_58eeafa5256e97d4e58e872b283551b655104e1f`)
 const connector = ArcanaConnector({
-  auth,
+    auth: auth as any
 })
 
 export const config = createConfig({
