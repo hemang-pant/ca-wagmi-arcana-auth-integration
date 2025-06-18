ef605abd8e1eebb3d98a744a292b094a58db5825
hemang <hemangpant2002@gmail.com>
2025-06-18 13:04:29 +0530

    bumped sdk version

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -6,14 +6,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { config } from "./utils/config";
 import { CAProvider } from "@arcana/ca-wagmi";
+import { CA } from "@arcana/ca-sdk";
 
 const queryClient = new QueryClient();
+const ca = new CA();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <CAProvider>
+        <CAProvider client={ca}>
           <App />
         </CAProvider>
       </QueryClientProvider>
